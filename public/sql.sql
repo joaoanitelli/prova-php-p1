d10693a5a1b33bda76ab657d9434ee2d06a990f1
joaoanitelli <133887229+joaoanitelli@users.noreply.github.com>
2024-04-20 09:39:06 -0300

    Update sql.sql

--- public/sql.sql
+++ public/sql.sql
@@ -8,7 +8,7 @@ CREATE TABLE carro (
   ano int NOT NULL,
   cor VARCHAR(255) NOT NULL,
   valor float NOT NULL,
-  PRIMARY KEY (`id`));
+  PRIMARY KEY (id));
   
 CREATE TABLE cliente (
   id INT NOT NULL AUTO_INCREMENT,
@@ -16,7 +16,7 @@ CREATE TABLE cliente (
   cpf int NOT NULL,
   cidade VARCHAR(255) NOT NULL,
   estado VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`));
+  PRIMARY KEY (id));
     
 CREATE TABLE filial (
   id INT NOT NULL AUTO_INCREMENT,
@@ -24,7 +24,7 @@ CREATE TABLE filial (
   tel int NOT NULL,
   cidade VARCHAR(255) NOT NULL,
   estado VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`));
+  PRIMARY KEY (id));
       
 CREATE TABLE fornecedor (
   id INT NOT NULL AUTO_INCREMENT,
@@ -32,6 +32,6 @@ CREATE TABLE fornecedor (
   tel int NOT NULL,
   cidade VARCHAR(255) NOT NULL,
   estado VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`));
+  PRIMARY KEY (id));
   
   
